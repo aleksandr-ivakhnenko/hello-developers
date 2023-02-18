de446cb42bd8828a46d9590dc11003b0bfc7a910
aleksandr-ivakhnenko <125494676+aleksandr-ivakhnenko@users.noreply.github.com>
2023-02-18 16:01:41 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,34 +1,54 @@
-const btnClose = document.querySelector('.js-btn-close');
+const btnMenuClose = document.querySelector('.js-btn-menu-close');
 const btnMenu = document.querySelector('.js-btn-menu');
 const navMenu = document.querySelector('.js-nav-menu');
-const menuLinkItem = document.querySelector('.js-link-item');
 
-const headerLogo = document.querySelector('.js-header-logo');
-const headerActions = document.querySelector('.js-header-actions');
-const main = document.querySelector('.js-main');
+const btnMentor = document.querySelector('.js-btn-mentor');
+const popupMentor = document.querySelector('.js-popup-mentor');
+const btnPopupClose = document.querySelector('.js-btn-popup-close');
 
-
-const bodyDocument = document.body;
-const wrapper = document.querySelector('.js-wrapper');
+const overlay = document.querySelector('.js-overlay');
+const linkItemScroll = document.querySelector('.js-link-item-scroll');
+const bodyDocument = document.querySelector('body');
 
 function openMenu() {
-    bodyDocument.classList.add('open-nav-menu');
-    wrapper.classList.add('open-nav-menu');
-	navMenu.classList.add('active');
-    btnMenu.classList.add('active')
+    bodyDocument.classList.add('scroll-lock');
+	navMenu.classList.add('menu-open');
+    btnMenu.classList.add('menu-open');
+    overlay.classList.add('menu-open');
 };
 
 function closeMenu() {
-    bodyDocument.classList.remove('open-nav-menu');
-    wrapper.classList.remove('open-nav-menu');
-	navMenu.classList.remove('active');
-    btnMenu.classList.remove('active')
+    bodyDocument.classList.remove('scroll-lock');
+	navMenu.classList.remove('menu-open');
+    btnMenu.classList.remove('menu-open');
+    overlay.classList.remove('menu-open');
 };
 
-btnMenu.addEventListener('click', openMenu);
 
-btnClose.addEventListener('click', closeMenu);
-menuLinkItem.addEventListener('click', closeMenu);
-headerLogo.addEventListener('click', closeMenu);
-headerActions.addEventListener('click', closeMenu);
-main.addEventListener('click', closeMenu);
+function openPopup() {
+    bodyDocument.classList.add('scroll-lock');
+    popupMentor.classList.add('popup-open');
+    btnMentor.classList.add('popup-open');
+};
+
+function closePopup() {
+    bodyDocument.classList.remove('scroll-lock');
+    popupMentor.classList.remove('popup-open');
+    btnMentor.classList.remove('popup-open');
+};
+
+
+btnMenu.addEventListener('click', openMenu);
+btnMenuClose.addEventListener('click', closeMenu);
+linkItemScroll.addEventListener('click', closeMenu);
+
+overlay.addEventListener('click', closeMenu);
+
+btnMentor.addEventListener('click', function(e) {
+    e.preventDefault()
+    openPopup()
+});
+
+btnPopupClose.addEventListener('click', closePopup);
+
+overlay.addEventListener('click', closePopup);
